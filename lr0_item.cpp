5a8682efab9fe12e10e97b8f5b5b92b1299c6eb2
jose-rZM <100773386+jose-rZM@users.noreply.github.com>
2025-02-14 09:57:21 +0100

    eol and epsilon stored in each lr0item

--- lr0_item.cpp
+++ lr0_item.cpp
@@ -8,70 +8,88 @@
 #include "lr0_item.hpp"
 #include "symbol_table.hpp"
 
-Lr0Item::Lr0Item(std::string antecedent, std::vector<std::string> consequent)
-    : antecedent(std::move(antecedent)), consequent(std::move(consequent)) {
-    if (this->consequent.size() == 1 && this->consequent[0] == "EPSILON") {
-        dot = 1;
+Lr0Item::Lr0Item(std::string antecedent, std::vector<std::string> consequent,
+                 std::string epsilon, std::string eol)
+    : antecedent_(std::move(antecedent)), consequent_(std::move(consequent)),
+      epsilon_(std::move(epsilon)), eol_(std::move(eol)), dot_(0) {
+    if (this->consequent_.size() == 1 && this->consequent_[0] == epsilon_) {
+        dot_ = 1;
     }
 }
 
 Lr0Item::Lr0Item(std::string antecedent, std::vector<std::string> consequent,
-                 unsigned int dot)
-    : antecedent(std::move(antecedent)), consequent(std::move(consequent)),
-      dot(dot) {
-    if (this->consequent.size() == 1 && this->consequent[0] == "EPSILON") {
+                 unsigned int dot, std::string epsilon, std::string eol)
+    : antecedent_(std::move(antecedent)), consequent_(std::move(consequent)),
+      epsilon_(std::move(epsilon)), eol_(std::move(eol)), dot_(dot) {
+    if (this->consequent_.size() == 1 && this->consequent_[0] == epsilon_) {
         dot = 1;
     }
 }
 
-std::string Lr0Item::nextToDot() const {
-    if (dot == consequent.size() ||
-        (dot < consequent.size() && consequent[dot] == "$")) {
-        return "EPSILON";
+std::string Lr0Item::NextToDot() const {
+    if (dot_ == consequent_.size() ||
+        (dot_ < consequent_.size() && consequent_[dot_] == eol_)) {
+        return epsilon_;
     }
-    return consequent[dot];
+    return consequent_[dot_];
 }
 
-void Lr0Item::advanceDot() {
-    dot++;
+void Lr0Item::AdvanceDot() {
+    dot_++;
 }
 
-bool Lr0Item::isComplete() const {
-    return dot >= consequent.size() ||
-           (consequent.size() == 1 && consequent[0] == "EPSILON") ||
-           (dot < consequent.size() && consequent[dot] == "$");
+bool Lr0Item::IsComplete() const {
+    return dot_ >= consequent_.size() ||
+           (consequent_.size() == 1 && consequent_[0] == epsilon_) ||
+           (dot_ < consequent_.size() && consequent_[dot_] == eol_);
 }
 
-void Lr0Item::printItem() const {
-    std::cout << "[ " << antecedent << " -> ";
-    for (unsigned int i = 0; i < consequent.size(); ++i) {
-        if (i == dot) {
-            std::cout << "<*> ";
+void Lr0Item::PrintItem() const {
+    std::cout << "[ " << antecedent_ << " -> ";
+    for (unsigned int i = 0; i < consequent_.size(); ++i) {
+        if (i == dot_) {
+            std::cout << "· ";
         }
-        std::cout << consequent[i] << " ";
+        std::cout << consequent_[i] << " ";
     }
-    if (dot == consequent.size()) {
-        std::cout << "<*>";
+    if (dot_ == consequent_.size()) {
+        std::cout << "· ";
     }
     std::cout << "]";
 }
 
+std::string Lr0Item::ToString() const {
+    std::string str = "[ " + antecedent_ + " -> ";
+    for (size_t i = 0; i < consequent_.size(); ++i) {
+        if (i == dot_) {
+            str += "· ";
+        }
+        str += consequent_[i] + " ";
+    }
+    if (dot_ == consequent_.size()) {
+        str += "· ";
+    }
+    str += "]";
+    return str;
+}
+
 bool Lr0Item::operator==(const Lr0Item& other) const {
-    return antecedent == other.antecedent && consequent == other.consequent &&
-           dot == other.dot;
+    return antecedent_ == other.antecedent_ &&
+           consequent_ == other.consequent_ && dot_ == other.dot_;
 }
 
 namespace std {
 std::size_t hash<Lr0Item>::operator()(const Lr0Item& item) const {
     std::size_t seed = 0;
 
-    seed ^= std::hash<std::string>()(item.antecedent) + 0x9e3779b9 +
+    seed ^= std::hash<std::string>()(item.antecedent_) + 0x9e3779b9 +
             (seed << 6) + (seed >> 2);
-    for (const auto& str : item.consequent) {
+    for (const auto& str : item.consequent_) {
         seed ^= std::hash<std::string>()(str) + 0x9e3779b9 + (seed << 6) +
                 (seed >> 2);
     }
-    seed ^= std::hash<int>()(item.dot) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    seed ^=
+        std::hash<int>()(item.dot_) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 
     return seed;
 }
