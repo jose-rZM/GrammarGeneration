e4713e2ab1e657d11c2e62aa04681d2275f6660a
jose-rZM <100773386+jose-rZM@users.noreply.github.com>
2025-02-14 09:58:02 +0100

    naming

--- state.hpp
+++ state.hpp
@@ -1,4 +1,4 @@
-
+#pragma once
 #include "lr0_item.hpp"
 #include <algorithm>
 #include <cstddef>
@@ -7,23 +7,17 @@
 #include <unordered_set>
 
 struct state {
-    std::unordered_set<Lr0Item> items;
-    unsigned int                id;
+    std::unordered_set<Lr0Item> items_;
+    unsigned int                id_;
 
-    bool operator==(const state& other) const { return other.items == items; }
+    bool operator==(const state& other) const { return other.items_ == items_; }
 };
 
 namespace std {
 template <> struct hash<state> {
     size_t operator()(const state& st) const {
-        /*size_t seed = std::accumulate(
-                st.items.begin(), st.items.end(), 0, [](size_t acc, const
-           Lr0Item& item) { return acc ^ (std::hash<Lr0Item>()(item) +
-           0x9e3779b9 + (acc << 6) + (acc >> 2));
-                }
-            );*/
         size_t seed =
-            std::accumulate(st.items.begin(), st.items.end(), 0,
+            std::accumulate(st.items_.begin(), st.items_.end(), 0,
                             [](size_t acc, const Lr0Item& item) {
                                 return acc ^ (std::hash<Lr0Item>()(item));
                             });
