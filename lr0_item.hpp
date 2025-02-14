488600fe112a30a5a756d23e95d54c1fe2f31e53
jose-rZM <100773386+jose-rZM@users.noreply.github.com>
2025-02-14 09:56:58 +0100

    change naming

--- lr0_item.hpp
+++ lr0_item.hpp
@@ -4,17 +4,21 @@
 #include <vector>
 
 struct Lr0Item {
-    std::string              antecedent;
-    std::vector<std::string> consequent;
-    unsigned int             dot = 0;
+    std::string              antecedent_;
+    std::vector<std::string> consequent_;
+    std::string              epsilon_;
+    std::string              eol_;
+    unsigned int             dot_ = 0;
 
-    Lr0Item(std::string antecedent, std::vector<std::string> consequent);
     Lr0Item(std::string antecedent, std::vector<std::string> consequent,
-            unsigned int dot);
-    std::string nextToDot() const;
-    void        printItem() const;
-    void        advanceDot();
-    bool        isComplete() const;
+            std::string epsilon, std::string eol);
+    Lr0Item(std::string antecedent, std::vector<std::string> consequent,
+            unsigned int dot, std::string epsilon, std::string eol);
+    std::string NextToDot() const;
+    void        PrintItem() const;
+    std::string ToString() const;
+    void        AdvanceDot();
+    bool        IsComplete() const;
     bool        operator==(const Lr0Item& other) const;
 };
 
